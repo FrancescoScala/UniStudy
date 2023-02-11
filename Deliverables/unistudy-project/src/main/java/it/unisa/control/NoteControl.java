f3fb2baa0bf98dd3e58aba714f9758c9e6ada82d
francesco <fscala001@gmail.com>
2023-02-11 18:20:48 +0100

    Add removeNote front-end for the role GestoreCorso

--- Deliverables/unistudy-project/src/main/java/it/unisa/control/NoteControl.java
+++ Deliverables/unistudy-project/src/main/java/it/unisa/control/NoteControl.java
@@ -13,6 +13,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Timestamp;
+import java.util.Set;
 
 @WebServlet(name = "NoteControl", value = "/NoteControl")
 public class NoteControl extends HttpServlet {
@@ -23,17 +24,17 @@ public class NoteControl extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        switch (request.getParameter("action")){
+        switch (request.getParameter("action")) {
             case "add":
                 response.setContentType("application/json");
                 PrintWriter out = response.getWriter();
                 Course course = CourseManager.retrieveCourseById(Integer.parseInt(request.getParameter("id")));
                 User author = (User) request.getSession().getAttribute("userInSession");
-                System.out.println("Dati arrivati a addNote: "+
-                        request.getParameter("description")+
-                        request.getParameter("title")+
-                        author.getId()+
-                        author.getName()+author.getSurname()+
+                System.out.println("Dati arrivati a addNote: " +
+                        request.getParameter("description") +
+                        request.getParameter("title") +
+                        author.getId() +
+                        author.getName() + author.getSurname() +
                         course);
 
                 boolean check = NoteManager.createNote(request.getParameter("description"),
@@ -41,39 +42,56 @@ public class NoteControl extends HttpServlet {
                         "/ciao/filepath.img",
                         request.getParameter("title"),
                         author.getId(),
-                        author.getName()+" "+author.getSurname(),
+                        author.getName() + " " + author.getSurname(),
                         course);
                 String mex;
-                if(check) {
+                if (check) {
                     mex = "OK";
-                }
-                else {
+                } else {
                     mex = "Aggiunta nota fallita. Reinsirisci i dati nel formato errato corretto e riprova";
                 }
                 JSONObject json = new JSONObject();
-                json.put("result",mex);
-                System.out.println("addNote restituisce: "+mex);
+                json.put("result", mex);
+                System.out.println("addNote restituisce: " + mex);
                 out.print(json.toString());
                 break;
 
             case "delete":
-                User user = (User) request.getSession().getAttribute("userInSession");
-                int noteAuthorId = Integer.parseInt(request.getParameter("noteAuthorId"));
-                if (((User) request.getSession().getAttribute("userInSession")).getId() == noteAuthorId) {
-                    boolean checkDelete = NoteManager.deleteNote(Integer.parseInt(request.getParameter("noteId")));
-                    if(checkDelete)
-                    {
-                        int id = Integer.parseInt(request.getParameter("id"));
-                        Course course1 = CourseManager.retrieveCourseById(id);
-                        request.setAttribute("course", course1);
-                        request.getRequestDispatcher("/partecipante/corso/view_course.jsp?id=" + course1.getId() + "").forward(request, response);
+                String askerRole = request.getParameter("role");
+                if (askerRole == null) {
+                    User user = (User) request.getSession().getAttribute("userInSession");
+                    int noteAuthorId = Integer.parseInt(request.getParameter("noteAuthorId"));
+                    if (((User) request.getSession().getAttribute("userInSession")).getId() == noteAuthorId) {
+                        boolean checkDelete = NoteManager.deleteNote(Integer.parseInt(request.getParameter("noteId")));
+                        if (checkDelete) {
+                            int id = Integer.parseInt(request.getParameter("id"));
+                            Course course1 = CourseManager.retrieveCourseById(id);
+                            request.setAttribute("course", course1);
+                            request.getRequestDispatcher("/partecipante/corso/view_course.jsp?id=" + course1.getId() + "").forward(request, response);
+                        }
+                        // else pagina di errore
                     }
-                    // else pagina di errore
+                } else {
+                    boolean checkDelete1 = NoteManager.deleteNote(Integer.parseInt(request.getParameter("noteId")));
+                    if (checkDelete1) {
+                        int id = Integer.parseInt(request.getParameter("id"));
+                        Set<Note> notes = NoteManager.retrieveNotesByCourseId(id);
+                        System.out.println(notes);
+                        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/partecipante/corso/gestore/remove-note.jsp?id=" + id);
+                        request.setAttribute("notes", notes);
+                        dispatcher.forward(request, response);
+                    }
                 }
                 break;
 
             case "view":
-
+                System.out.println("Sono in note view");
+                Set<Note> notes = NoteManager.retrieveNotesByCourseId(Integer.parseInt(request.getParameter("id")));
+                System.out.println("Note:" + notes);
+                RequestDispatcher dispatcher = this.getServletContext()
+                        .getRequestDispatcher("/partecipante/corso/gestore/remove-note.jsp?id=" + request.getParameter("id"));
+                request.setAttribute("notes", notes);
+                dispatcher.forward(request, response);
                 break;
             default:
         }
